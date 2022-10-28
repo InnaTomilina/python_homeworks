529c99a4a095c15cc2e0e3e3d37750f820276621
Inna Tomilina <krutas.inna@gmail.com>
2022-10-28 20:14:23 +0100

    upd: task 2

--- lesson_2/task_2.py
+++ lesson_2/task_2.py
@@ -1,1 +1,9 @@
-print("hello world")
+# output - several parameters separated by %%%
+print(1, 5, 234, "a", sep="%%%")
+
+# output - several parameters separated by commas
+print("a", "b", "c", "d", sep=",")
+
+# output - second print statement will be printed on a new line and indented by one line with "!" in the end
+print('Hello, Beetroot academy', end='\n\n')
+print('Hello, Beetroot academy', end='!')
