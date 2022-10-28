4cffed10a8f73c42ef3b1633ce70eade102cb236
Inna Tomilina <krutas.inna@gmail.com>
2022-10-28 20:13:46 +0100

    add: task 3

---
+++ lesson_2/task_3.py
@@ -0,0 +1,10 @@
+# capital letters "O"
+print("#" * 9)
+print("#", "#", "#", "#", sep="\t \t#\n", end="#" * 8)
+
+# print to separated letters
+print("\n\n")
+
+# capital letters "H"
+print("#", "#", "#", sep="\t \t#\n", end="#" * 8)
+print("\n#", "#", sep="\t\t#\n", end="\t\t#")
