7051562410139f16c26e5fb0825a24b606fa36e9
Inna Tomilina <krutas.inna@gmail.com>
2022-10-29 22:25:23 +0100

    add: task1, task2, task3

---
+++ lesson_3/task_2.py
@@ -0,0 +1,5 @@
+#Manipulate strings
+
+first_name = "Inna"
+second_name = "Tomilina"
+print("Hi" + " " + first_name + " " + second_name + "!")
---
+++ lesson_3/task_3.py
@@ -0,0 +1,12 @@
+#Using python as a calculator
+
+a = 10
+b = 5
+
+print('addition -', a + b)
+print('subtraction -', a - b)
+print('division -', a / b)
+print('multiplication -', a * b)
+print('exponent -', a ** b)
+print('modulus -', a % b)
+print('floor division -', a // b)
